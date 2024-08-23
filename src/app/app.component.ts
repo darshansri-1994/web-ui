a2b893d6c8024225be8dee5ba5d9f6fd6d2c3c7c
RaghuManu <raghukariappa@gmail.com>
2024-08-23 08:49:21 +0530

    api integration updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-app-5';
+  users = []
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getData().subscribe((res:any)=>{
+      this.users = res['data'];
+      console.log("users",this.users)
+    })
+  }
 }
